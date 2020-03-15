b686ac2541ad8bb3719638e17ffec115a8ea0a79
Alfredo Artiles <aartiles@gmail.com>
2020-03-15 13:21:24 +0100

    Get statuses data from github repo

--- lambda/custom/src/app/actionsFactory.js
+++ lambda/custom/src/app/actionsFactory.js
@@ -2,7 +2,7 @@
 const getStatusByCityFactory = require('../core/actions/getStatusByCity');
 const StatusesRetrieverFromURLFactory = require('../core/infrastructure/StatusesRetrieverFromURL');
 const StatusesRepositoryFactory = require('../core/infrastructure/StatusesRepository');
-const STATUSES_URL = 'https://gist.githubusercontent.com/aartiles/9e3d8fc7864109d96e8acd41efd99e50/raw/08746a9fa10f54ca5a31cec6186328d2e6c2be0a/covid-19-spain-by-province.json';
+const STATUSES_URL = 'https://raw.githubusercontent.com/aartiles/coronavirus-status-skill/master/lambda/custom/data/statuses.json';
 const statusesRetriever = StatusesRetrieverFromURLFactory({ url: STATUSES_URL });
 
 module.exports = () => {
